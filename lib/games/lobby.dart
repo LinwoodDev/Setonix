aec443a5dd657ec048065305a414bba688b86c23
CodeDoctorDE <wusel905@gmail.com>
2020-11-03 05:18:01 +0100

    change language files, add lobby game mode

--- lib/games/lobby.dart
+++ lib/games/lobby.dart
@@ -1,1 +1,37 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:minigamesparty/services/game/mode.dart';
+import 'package:minigamesparty/services/game/system.dart';
+import 'package:minigamesparty/services/game/player.dart';
 
+class LobbyGameMode extends GameMode {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+  @override
+  void join(Player player) {
+    // TODO: implement join
+  }
+
+  @override
+  void leave(Player player) {
+    // TODO: implement leave
+  }
+
+  @override
+  void read(Player player, String message) {
+    // TODO: implement read
+  }
+
+  @override
+  void start(GameSystem game) {
+    // TODO: implement start
+  }
+
+  @override
+  void stop() {
+    // TODO: implement stop
+  }
+}
