b53da020d2f1e3a24d02b122aad0a403650b95ec
CodeDoctorDE <wusel905@gmail.com>
2020-11-08 17:18:12 +0100

    finish colors

--- lib/colors.dart
+++ lib/colors.dart
@@ -5,7 +5,7 @@ class PaletteColors {
   static const int primary = 0xFFDB4900;
   static const Color primaryColor = const Color(primary);
   static const int secondary = 0xFFFFE605;
-  static const Color secondaryColor = const Color();
+  static const Color secondaryColor = const Color(secondary);
   static const Color accentColor = const Color(0xFFFA003E);
   static const Color lightColor = const Color(0xFFFFDFB3);
   static const Color darkColor = const Color(0xFF660A00);
