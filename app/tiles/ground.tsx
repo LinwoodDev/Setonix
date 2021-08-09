3b911297114dc2229c53ee282ae2483454dc5f52
CodeDoctorDE <wusel905@gmail.com>
2021-08-09 16:00:17 +0200

    add server and shared

--- app/tiles/ground.tsx
+++ app/tiles/ground.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.6.0" name="ground" tilewidth="64" tileheight="64" tilecount="16" columns="4">
+<tileset version="1.5" tiledversion="1.7.1" name="ground" tilewidth="64" tileheight="64" tilecount="16" columns="4">
  <image source="ground.png" width="256" height="256"/>
 </tileset>
--- app/tiles/road1.tsx
+++ app/tiles/road1.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.6.0" name="road1" tilewidth="64" tileheight="64" tilecount="16" columns="4">
+<tileset version="1.5" tiledversion="1.7.1" name="road1" tilewidth="64" tileheight="64" tilecount="16" columns="4">
  <image source="road1.png" width="256" height="256"/>
 </tileset>
