a22b4e96ad3d5e676c21c9ef06f85422f64d152d
CodeDoctorDE <codedoctor@linwood.dev>
2023-07-02 17:35:47 +0200

    Add new home page

--- app/lib/pages/home/page.dart
+++ app/lib/pages/home/page.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:go_router/go_router.dart';
-import 'package:material_leap/material_leap.dart';
 import 'package:phosphor_flutter/phosphor_flutter.dart';
 import 'package:qeck/main.dart';
 import 'package:qeck/models/server.dart';
@@ -22,81 +21,130 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: WindowTitleBar(
-          title: const Text(shortApplicationName),
-          actions: [
-            IconButton(
-              icon: const PhosphorIcon(PhosphorIconsLight.gear),
-              onPressed: () => openSettings(context),
-            )
-          ],
-        ),
-        body: StreamBuilder<List<GameServer>>(
-          stream: context.read<ConnectionService>().fetchServers(),
-          builder: (context, snapshot) {
-            if (snapshot.hasError) {
-              return Center(
-                child: Text(snapshot.error.toString()),
-              );
-            }
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-            final servers = snapshot.data ?? [];
-            return ListView.builder(
-              itemCount: servers.length,
-              itemBuilder: (context, index) {
-                final server = servers[index];
-                return ListTile(
-                  title: Text(server.property.name),
-                  subtitle: Text(server.address),
-                );
-              },
-            );
-          },
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () => showLeapBottomSheet(
-            context: context,
-            title: AppLocalizations.of(context).servers,
-            childrenBuilder: (context) => [
-              ListTile(
-                title: Text(AppLocalizations.of(context).connect),
-                onTap: () async {
-                  final address = await showDialog<Uri>(
-                    context: context,
-                    builder: (context) => const ConnectGameDialog(),
-                  );
-                  if (address == null) return;
-                  if (context.mounted) {
-                    final connection = ClientGameConnection.connect(address);
-                    context.push('/game', extra: connection);
-                  }
-                },
-              ),
-              if (!kIsWeb)
-                ListTile(
-                  title: Text(AppLocalizations.of(context).create),
-                  onTap: () async {
-                    final property = await showDialog<GameProperty>(
-                      context: context,
-                      builder: (context) => const CreateGameDialog(),
-                    );
-                    if (property == null) return;
-                    if (context.mounted) {
-                      final connection =
-                          await ClientServerGameConnection.create();
-                      if (context.mounted) {
-                        context.push('/game', extra: connection);
-                      }
+      appBar: WindowTitleBar(
+        title: const Text(shortApplicationName),
+        actions: [
+          IconButton(
+            icon: const PhosphorIcon(PhosphorIconsLight.gear),
+            onPressed: () => openSettings(context),
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Align(
+          child: Container(
+            padding: const EdgeInsets.all(16),
+            constraints: const BoxConstraints(maxWidth: 600),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  applicationName,
+                  style: Theme.of(context).textTheme.displayMedium,
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 16),
+                Wrap(
+                    alignment: WrapAlignment.center,
+                    children: [
+                      (
+                        AppLocalizations.of(context).connect,
+                        PhosphorIconsLight.globe,
+                        () async {
+                          final address = await showDialog<Uri>(
+                            context: context,
+                            builder: (context) => const ConnectGameDialog(),
+                          );
+                          if (address == null) return;
+                          if (context.mounted) {
+                            final connection =
+                                ClientGameConnection.connect(address);
+                            context.push('/game', extra: connection);
+                          }
+                        },
+                      ),
+                      if (!kIsWeb)
+                        (
+                          AppLocalizations.of(context).create,
+                          PhosphorIconsLight.plus,
+                          () async {
+                            final property = await showDialog<GameProperty>(
+                              context: context,
+                              builder: (context) => const CreateGameDialog(),
+                            );
+                            if (property == null) return;
+                            if (context.mounted) {
+                              final connection =
+                                  await ClientServerGameConnection.create();
+                              if (context.mounted) {
+                                context.push('/game', extra: connection);
+                              }
+                            }
+                          },
+                        ),
+                    ]
+                        .map((e) => SizedBox(
+                              height: 200,
+                              width: 200,
+                              child: Card(
+                                  clipBehavior: Clip.antiAlias,
+                                  margin: const EdgeInsets.all(8),
+                                  child: InkWell(
+                                    onTap: e.$3,
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(16.0),
+                                      child: Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: [
+                                          Icon(e.$2, size: 48),
+                                          const SizedBox(height: 16),
+                                          Text(
+                                            e.$1,
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .headlineSmall,
+                                            textAlign: TextAlign.center,
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  )),
+                            ))
+                        .toList()),
+                StreamBuilder<List<GameServer>>(
+                  stream: context.read<ConnectionService>().fetchServers(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasError) {
+                      return Center(
+                        child: Text(snapshot.error.toString()),
+                      );
                     }
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    }
+                    final servers = snapshot.data ?? [];
+                    return ListView.builder(
+                      itemCount: servers.length,
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemBuilder: (context, index) {
+                        final server = servers[index];
+                        return ListTile(
+                          title: Text(server.property.name),
+                          subtitle: Text(server.address),
+                        );
+                      },
+                    );
                   },
                 ),
-            ],
+              ],
+            ),
           ),
-          child: const Icon(Icons.add),
-        ));
+        ),
+      ),
+    );
   }
 }
