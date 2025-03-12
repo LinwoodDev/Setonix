db50181bdddbf3ad64eac62d1ced2b73d82c9c4b
CodeDoctorDE <codedoctor@linwood.dev>
2025-03-12 23:19:12 +0100

    Fix settings dialog

--- app/lib/api/settings.dart
+++ app/lib/api/settings.dart
@@ -1,19 +1,34 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 import '../pages/settings/page.dart';
 
-Future<void> openSettings(
-  BuildContext context, {
-  SettingsView view = SettingsView.general,
-}) =>
-    showDialog<void>(
-        context: context,
-        builder: (context) => Dialog(
-            clipBehavior: Clip.antiAlias,
-            child: ConstrainedBox(
-                constraints:
-                    const BoxConstraints(maxHeight: 600, maxWidth: 800),
-                child: SettingsPage(
-                  isDialog: true,
-                  view: view,
-                ))));
+Future<void> openSettings(BuildContext context) => showGeneralDialog<void>(
+      context: context,
+      pageBuilder: (context, animation, secondaryAnimation) =>
+          ScaffoldMessenger(
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+          child: Dialog(
+              clipBehavior: Clip.antiAlias,
+              child: ConstrainedBox(
+                  constraints:
+                      const BoxConstraints(maxHeight: 800, maxWidth: 1000),
+                  child: const SettingsPage(isDialog: true))),
+        ),
+      ),
+      barrierDismissible: true,
+      barrierLabel: MaterialLocalizations.of(context).modalBarrierDismissLabel,
+      transitionDuration: const Duration(milliseconds: 200),
+      transitionBuilder: (context, animation, secondaryAnimation, child) {
+        // Animate the dialog from bottom to center
+        return SlideTransition(
+          position: Tween<Offset>(
+            begin: const Offset(0, 1),
+            end: Offset.zero,
+          ).chain(CurveTween(curve: Curves.easeOutQuart)).animate(animation),
+          child: child,
+        );
+      },
+    );
