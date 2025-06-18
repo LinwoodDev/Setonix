c07a8393396234a85a7cb96676654238071d73a2
CodeDoctorDE <codedoctor@linwood.dev>
2025-06-18 15:51:35 +0200

    Fix cors

--- servers/src/pages/data.json.ts
+++ servers/src/pages/data.json.ts
@@ -1,10 +1,24 @@
-import type { APIRoute } from 'astro';
-import { loadServersFromConfig } from '../scripts/servers/list';
+import type { APIRoute } from "astro";
+import { loadServersFromConfig } from "../scripts/servers/list";
 
 export async function GET() {
   const servers = await loadServersFromConfig();
-  return new Response(JSON.stringify({servers}), {
+  return new Response(JSON.stringify({ servers }), {
     status: 200,
-    headers: { 'Content-Type': 'application/json' },
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+    },
+  });
+}
+
+export const OPTIONS: APIRoute = async () => {
+  return new Response(null, {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET",
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Max-Age": "21600", // 6 h cache
+    },
   });
 };
